c958775d0f6cadc0a6762557fafad4d0d5a5df13
Sebastian Neumann <sebastian@bumpern.de>
2013-02-11 12:17:45 +0100

    configure actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,10 @@ Dursty::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from:'no-reply@bier.oph.rwth-aachen.de'}
 
   # Enable threaded mode
   # config.threadsafe!
