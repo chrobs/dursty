92c9be3323fc5c65597ab15f9483a2f8d8e75c37
Sebastian Neumann <sebastian@bumpern.de>
2013-02-11 12:39:33 +0100

    try to fix actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,8 @@ Dursty::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from:'no-reply@bier.oph.rwth-aachen.de'}
+  #config.action_mailer.default_options = {from:'no-reply@bier.oph.rwth-aachen.de'}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Enable threaded mode
   # config.threadsafe!
