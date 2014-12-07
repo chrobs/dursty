4c611245bbeca0338905b6749c3bc881805899fc
Sebastian Neumann <sebastian@bumpern.de>
2014-12-07 11:19:41 +0100

    remove secret token from version control

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dursty::Application.config.secret_token = 'd27b21641141a9d6a99a584b176d51797f21aa2b55145b968e8020e5f03c1206bf2d4f668a3d958a0577d4450b0ca4acbf55cf560a18993d212fcb0b98a488da'
